d34049ac1d80fbe1eb9845bbd2e5c8469489f470
AKramarenko <kkramarenkoo@icloud.com>
2025-10-04 15:01:12 +0300

    загрузка файлов в репозиторий

---
+++ practice1.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# загружаем датасет из папки, в которой находится скрипт
+df = pd.read_csv('practice_1.csv', delimiter=';')
+
+# задаем названия колонок и их расшифровку значений для вывода пользователю
+question_descriptions = {
+    'PhoneType': 'Какой у вас телефон? (Айфон - 1, Андроид - 2)',
+    'Gender': 'Пол (М - 1, Ж - 2)',
+    'SiblingsCount': 'Количество братьев/сестёр (укажите цифру)',
+    'OS_PC': 'ОС на ПК (MacOS - 1, Windows - 2, Linux - 3)',
+    'TaxiTripsPerMonth': 'Среднее кол-во поездок на такси в месяц (укажите цифру)',
+    'MobileGames': 'Играете в мобильные игры? (Да -1, Нет - 2)',
+    'ResidenceArea': 'Область проживания (1-7, по списку)',
+    'CameraQuality': 'Важно ли качество камеры? (Да - 1, Нет - 2)',
+    'OriginRegion': 'ФО (цифры в порядке расположения на картинке)',
+    'PaymentMethod': 'Чаще оплачиваете покупки... (1-5)',
+    'PhoneChangeFrequencyYears': 'Как часто меняете телефон? (укажите среднее количество лет)',
+    'EmploymentStatus': 'Ваше положение (Безработный - 1, Частная компания - 2, Госкомпания - 3)',
+    'SmartHomeUsage': 'Пользуетесь ли технологией умного дома? (Да - 1, Нет - 2)',
+    'ITField': 'Сфера работы IT? (Да - 1, Нет - 2)',
+    'WatchType': 'Какие часы? (Нет часов - 1, Механические - 2, Электронные - 3)',
+    'MaxBudget': 'Максимальный бюджет (число без пробелов)',
+    'DailyChargeCount': 'Сколько раз в день заряжаешь телефон? (цифра)',
+    'PreferredBrowser': 'Чем браузером чаще пользуешься? (Google - 1, Яндекс - 2, Safari - 3, Opera - 4, Edge - 5, Firefox - 6)',
+    'TechLovingScale': 'Любите ли вы новые технологии? (1-5)',
+    'UISettingsImportance': 'Важна ли настройка интерфейса? (Да - 1, Нет - 2)',
+    'MaterialQualityImportance': 'Важно ли качество материалов? (Да - 1, Нет - 2)'
+}
+
+# разделяем данные на обучающие и тестовые выборки
+X = df.drop('PhoneType', axis=1)
+y = df['PhoneType']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# обучение модели
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+
+print("Пожалуйста, ответьте на следующие вопросы:")
+input_features = []
+
+# вывод вопроса, ввод ответа, проверка на корректность введенного значения
+for feature in X.columns:
+    question = question_descriptions.get(feature, f"{feature}: ")
+    while True:
+        try:
+            value = input(f"{question}\n")
+            input_value = float(value)
+            input_features.append(input_value)
+            break
+        except ValueError:
+            print("Пожалуйста, введите корректное числовое значение.")
+
+# создаем DataFrame для предсказания
+input_df = pd.DataFrame([input_features], columns=X.columns)
+
+# предсказание
+prediction = knn.predict(input_df)
+
+# вывод результата
+print("\n\n", end='')
+if prediction[0] == 1:
+    print("\033[1mПредположительный телефон: Айфон\033[0m")
+elif prediction[0] == 2:
+    print("\033[1mПредположительный телефон: Андроид\033[0m")
+else:
+    print("\033[1mНеизвестное предсказание\033[0m")
